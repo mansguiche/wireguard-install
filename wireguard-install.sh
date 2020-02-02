ba5eef11203b5398bfe279a1a91b0c8c9897d12c
Luke <lmanson93@gmail.com>
2020-02-02 21:05:38 +0000

    Add servers own IP as a DNS option

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -69,7 +69,8 @@ if [ ! -f "$WG_CONFIG" ]; then
         echo "   1) Cloudflare"
         echo "   2) Google"
         echo "   3) OpenDNS"
-        read -p "DNS [1-3]: " -e -i 1 DNS_CHOICE
+	echo "   4) This Server ($SERVER_HOST)"
+        read -p "DNS [1-4]: " -e -i 1 DNS_CHOICE
 
         case $DNS_CHOICE in
             1)
@@ -81,6 +82,8 @@ if [ ! -f "$WG_CONFIG" ]; then
             3)
             CLIENT_DNS="208.67.222.222,208.67.220.220"
             ;;
+	    4)
+	    CLIENT_DNS="$SERVER_HOST"
         esac
     fi
 
